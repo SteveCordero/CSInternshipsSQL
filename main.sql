d0c5f0ab75e54fc504aab92ac25bb591fbc55376
Steve Cordero <77298189+SteveCordero@users.noreply.github.com>
2022-05-20 08:59:54 +0000

    Initial commit

---
+++ main.sql
@@ -0,0 +1,12 @@
+CREATE TABLE internships
+(
+  title TEXT,
+  deadline TEXT,
+  duration TEXT,
+  description TEXT,
+  paid BOOLEAN,
+  educationReq TEXT,
+  ageReq INT,
+  location TEXT,
+  company TEXT,
+);

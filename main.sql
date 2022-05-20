e6c8661571a85f663fe3be054212153220c14b54
Steve Cordero <77298189+SteveCordero@users.noreply.github.com>
2022-05-20 09:11:28 +0000

    Added 1 internship into the database and printed it out

--- main.sql
+++ main.sql
@@ -8,5 +8,13 @@ CREATE TABLE internships
   educationReq TEXT,
   ageReq INT,
   location TEXT,
-  company TEXT,
+  company TEXT
 );
+
+INSERT INTO internships values
+('CTG Computer Science Intern', 'none listed', '2022 Summer', 'Data Science Intern, experience gained in analysis of business environment, essentially business analysis', false, 'High School Diploma/GED , current Sophomore or higher in CS field or related field and Proficient in Python', 0, 'remote working', 'Computer Task Group inc.');
+
+
+.print
+.print 'Internships'
+SELECT * FROM internships

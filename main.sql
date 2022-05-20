058196aa3f676d1926c79f90df7e91ca37de4180
Steve Cordero <77298189+SteveCordero@users.noreply.github.com>
2022-05-20 15:32:05 +0000

    Added two more internships into my Database

--- main.sql
+++ main.sql
@@ -1,7 +1,7 @@
 CREATE TABLE internships
 (
   title TEXT,
-  deadline TEXT,
+  deadline DATE,
   duration TEXT,
   description TEXT,
   paid BOOLEAN,
@@ -12,7 +12,10 @@ CREATE TABLE internships
 );
 
 INSERT INTO internships values
-('CTG Computer Science Intern', 'none listed', '2022 Summer', 'Data Science Intern, experience gained in analysis of business environment, essentially business analysis', false, 'High School Diploma/GED , current Sophomore or higher in CS field or related field and Proficient in Python', 0, 'remote working', 'Computer Task Group inc.');
+('CTG Computer Science Intern', 0, '2022 Summer', 'Data Science Intern, experience gained in analysis of business environment, essentially business analysis', false, 'High School Diploma/GED , current Sophomore or higher in CS field or related field and Proficient in Python', 0, 'remote working', 'Computer Task Group inc.'),
+('RCIT Network Operations - Internship',0, '1 to 2 year commitment', '30-40 hrs per week, working on maintaining networks (Wide Area Network, WAN) and Voice and Data Network', true, '3.0 minimum GPA required, Enrolled in Bachelors or Masters program during the internship' , 0, 'Remote and in-office work, based outside of Riverside', 'The County of Riverside'),
+('Information Technology/Systems Intern', 0, '6 months, 12 - 15 hours per week', 'Set up and maintenance of computer and peripherals, Assistance with application development and Data Center operations', false , '2 years of undergraduate work, major in computer science or relating fields, or be a recent graduate completing degree in such fields', 0, 'none listed', 'Orange County Sheriffs department');
+
 
 
 .print

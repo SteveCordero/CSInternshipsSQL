cbee85c1f3c48bb7a85005a97f4b09c3d79fd9fd
Steve Cordero <77298189+SteveCordero@users.noreply.github.com>
2022-05-21 02:05:42 +0000

    added 9th internship in the internships database

--- main.sql
+++ main.sql
@@ -19,7 +19,8 @@ INSERT INTO internships values
 ('Computer SCience Intern - AAA', 0, '8 weeks, June to August', 'Complete research on members needs and design solutions, use data to identify opportunities, and collaboration with other departments', false, 'none listed', 0, 'Los Angeles, CA', 'American Automobile Association'),
 ('High School - Sri Web Development Intern', 0 , 'none listed', 'Connecting research and engineering to operations, data analytics, data acquisition/management' , false, ' sophormore/junior/senior in high school, cumulative GPA of 3.0+', 16, '902 Battelle Blvd, Richland, WA 99354', 'Pacific Northwest National laboratory'),
 ('Pathway to START Program Intership', 0 , 'initial 8 week program, otherwise not listed' , ' learning and understanding of all Tesla Vehicles, 40 hours a week, fundamentals of using basic hand tools', false, 'High School Diploma or GED', 18, 'Los Angeles, CA', 'Tesla Motors' ),
-('Software Engineering Intern - Rising Senior', 0 , '2022 summer, otherwise not listed' , 'Analysis, design, implementation, integration, and tes of embedded software for real-time and client server applications', false, '3.0 GPA minimum, Enrolled in Bachelors degree in Science, Technology or Mathematics', 0, 'Anaheim, CA', 'L3Harris');
+('Software Engineering Intern - Rising Senior', 0 , '2022 summer, otherwise not listed' , 'Analysis, design, implementation, integration, and tes of embedded software for real-time and client server applications', false, '3.0 GPA minimum, Enrolled in Bachelors degree in Science, Technology or Mathematics', 0, 'Anaheim, CA', 'L3Harris'),
+('Graduate Science Intern - Twitch', 0 , 'not listed', 'solve large-scale data problems, design solutions for Twitchs problem spaces, participate in talks',true , 'currently enrolled in a graduate program', 0,'Irvine, CA', ' Twitch');
 
 
 
